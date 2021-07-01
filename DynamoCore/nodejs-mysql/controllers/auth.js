941ff44d049ca28efedc013529ca6d00d72c9c5b
Derek-Stanley <71318168+Derek-Stanley@users.noreply.github.com>
2021-07-01 22:36:34 +0530

    cnt

--- DynamoCore/nodejs-mysql/controllers/auth.js
+++ DynamoCore/nodejs-mysql/controllers/auth.js
@@ -11,6 +11,26 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
  
+ exports.login = async (req,res) => {
+     try{
+         const email = req.body.email;
+         const pass = req.body.pass;
+
+         if(!email || !pass){
+            //send to fill pass and email
+         }
+
+         db.query('SELECT *From userinfo email = ?', [email], async (error,results) =>{
+             if(!results || !(await bcrypt.compare(password, results[0].password))) {
+                 //res.status(401).render(loginfile)
+             }
+         })
+
+     }catch(error) {
+         console.log(error);
+     }
+ 
+ } 
  exports.register = (req, res) => {
      console.log(req.body);
 
@@ -34,7 +54,7 @@ const db = mysql.createConnection({
     let hashedPassword = await bcrypt.hash(pass, 8);
     console.log(hashedPassword);
 
-    db.query('INSERT INTO userinfo SET ? ',{username: name, email: email, conactNo: contactNo, password: pass });
+    db.query('INSERT INTO userinfo SET ? ',{username: name, email: email, conactNo: contactNo, password: hashedPassword });
     if(error){
         console.log(error);
     } else {
